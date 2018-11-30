6439cd7bdce762d01ef95990178bc00fd5ceab02
KinvenD <37690427+KinvenD@users.noreply.github.com>
2018-11-30 19:28:40 +0700

    Create prog.c

---
+++ prog.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <crypt.h>
+
+int main()
+{
+  char *password = NULL;
+  char *hash = NULL;
+    
+  scanf("%ms",&password);
+  scanf("%ms",&hash);
+
+  char *cryptPassword = crypt(password, "salt");
+  if(cryptPassword == NULL){
+    printf("Error");
+    }else{
+      if(strcmp(cryptPassword, hash) == 0) {
+        printf("allow");
+      } else {
+        printf("deny");
+    }
+  }
+  free(password);
+  free(hash);
+  return 0;
+}
